de1411d4044041cb7fcd88eba1f37f634aa68ddf
Will Patterson <wlp223@aol.com>
2023-03-09 11:08:35 -0500

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,47 @@ console.log(
 );
 
 // Exercise 1
+const firstName = 'Will';
+console.log(`First Name: ${firstName}`);
+let lastName;
+var age;
 
 // Exercise 2
+lastName = 'Patterson';
+console.log(`Last Name: ${lastName}`);
+age = 30;
+console.log(`Age: ${age}`);
 
 // Exercise 3
+let language = 'JavaScript';
+console.log(`Language: ${language}`);
+let createdYear = 1995;
+console.log(`Year Created: ${createdYear}`);
+let isCaseSensitive = true;
+console.log(`Is Case Sensitive: ${isCaseSensitive}`);
+
+// Exercise 4
+let price = 19.99;
+console.log(`Price: ${price}`);
+let isOnSale = false;
+console.log(`On Sale: ${isOnSale}`);
+let salePercentage = 15;
+console.log(`Sale Percentage: ${salePercentage}`);
+let stock = 0;
+console.log(`Amount In Stock: ${stock}`);
+let inStock = false;
+console.log(`In Stock: ${inStock}`);
+let selectedSize = 'M';
+console.log(`Selected Size: ${selectedSize}`);
+
+// Exercise 5
+let title = 'Name of the Wind';
+console.log(`Title: ${title}`);
+let author = 'Patrick Rothfuss';
+console.log(`Author: ${author}`);
+let pageCount = 722;
+console.log(`Page Count: ${pageCount}`);
+let bookmark = 456;
+console.log(`Bookmark: ${bookmark}`);
+let hasRead = true;
+console.log(`Has Read: ${hasRead}`);
